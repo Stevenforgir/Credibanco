6fddb045ef23db5433b6962c5504139bdf0b9761
Stevenforgir <stevenforgir@gmail.com>
2022-11-11 11:04:43 -0500

    Backend and frontend Repository

---
+++ FrontCredibanco/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Lista de Pendientes</h1>
+      
+      <span>Pendientes: 03.</span>
+      
+      <ul className="card">
+        <li>Ver la sesi&oacute;n #06</li>
+        <li>Hacer el componente pr&aacute;ctico</li>
+        <li>Guardar c&aacute;mbios</li>
+      </ul>
+    </div>
+  );
+}
